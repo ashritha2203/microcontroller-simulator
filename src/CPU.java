31f1acfcbcc8d23fe2356742ddd233fd6affa9c7
ashritha2203 <kashritha345@gmail.com>
2026-09-07 09:44:56 +0530

    Update CPU.java

--- src/CPU.java
+++ src/CPU.java
@@ -1,26 +1,15 @@
 public class CPU {
-    // Working register (W) - PIC16F72 specific
     private int W;
-
-    // 8 General Purpose / File Registers
     private int[] registers;
-
-    // Program Counter
     private int pc;
-
-    // Flags
     private boolean zeroFlag;
     private boolean carryFlag;
-    private boolean dcFlag;   // Digit Carry - needed for PIC16F72
-
-    // Halt flag - needed to stop the Run loop
+    private boolean dcFlag;
     private boolean halted;
-
-    // Program Memory
     private String[] programMemory;
 
     public CPU() {
-        registers = new int[8];
+        registers = new int[256];
         programMemory = new String[256];
         reset();
     }
@@ -37,74 +26,23 @@ public class CPU {
         halted = false;
     }
 
-    // ---------- Getters ----------
-    public int[] getRegisters() {
-        return registers;
-    }
+    public int[] getRegisters() { return registers; }
+    public int getRegister(int index) { return registers[index]; }
+    public int getW() { return W; }
+    public int getPC() { return pc; }
+    public boolean getZeroFlag() { return zeroFlag; }
+    public boolean getCarryFlag() { return carryFlag; }
+    public boolean getDCFlag() { return dcFlag; }
+    public boolean isHalted() { return halted; }
+    public String[] getProgramMemory() { return programMemory; }
 
-    public int getRegister(int index) {
-        return registers[index];
-    }
+    public void setRegister(int index, int value) { registers[index] = value & 0xFF; }
+    public void setW(int value) { W = value & 0xFF; }
+    public void setZeroFlag(boolean value) { zeroFlag = value; }
+    public void setCarryFlag(boolean value) { carryFlag = value; }
+    public void setDCFlag(boolean value) { dcFlag = value; }
+    public void setHalted(boolean value) { halted = value; }
 
-    public int getW() {
-        return W;
-    }
-
-    public int getPC() {
-        return pc;
-    }
-
-    public boolean getZeroFlag() {
-        return zeroFlag;
-    }
-
-    public boolean getCarryFlag() {
-        return carryFlag;
-    }
-
-    public boolean getDCFlag() {
-        return dcFlag;
-    }
-
-    public boolean isHalted() {
-        return halted;
-    }
-
-    public String[] getProgramMemory() {
-        return programMemory;
-    }
-
-    // ---------- Setters (needed by instructions) ----------
-    public void setRegister(int index, int value) {
-        registers[index] = value & 0xFF;   // keep 8-bit
-    }
-
-    public void setW(int value) {
-        W = value & 0xFF;
-    }
-
-    public void setZeroFlag(boolean value) {
-        zeroFlag = value;
-    }
-
-    public void setCarryFlag(boolean value) {
-        carryFlag = value;
-    }
-
-    public void setDCFlag(boolean value) {
-        dcFlag = value;
-    }
-
-    public void setHalted(boolean value) {
-        halted = value;
-    }
-
-    // ---------- PC control ----------
-    public void incrementPC() {
-        pc++;
-    }
-
-    public void setPC(int address) {
-        pc = address;
-    }
+    public void incrementPC() { pc++; }
+    public void setPC(int address) { pc = address; }
 }
